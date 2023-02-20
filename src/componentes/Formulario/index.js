ea11f7249e55590e1406007950b7e517e1cadae9
Lea Sampaio <lealisboas@gmail.com>
2023-02-20 20:58:44 -0300

    update formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,15 +3,15 @@ import './Formulario.css'
 
 const Formulario = () => {
     return (
-        <section className="formulario">            
-            <form >
-                <h2> Preencha o formulário para criar o card do colaborador:</h2>
-                <CampoTexto label="Nome" placeholder= "Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-                <CampoTexto label="Imagem"placeholder="Digite o enderenço da imagem"/>
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
     )
-
 }
+
 export default Formulario
